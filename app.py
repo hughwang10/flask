67fc1e495110db8d2d8e22eefd488fe0d6d97eba
hughwang10 <hughwang10@gmail.com>
2017-10-04 13:11:50 +0100

    updates

--- app.py
+++ app.py
@@ -3,25 +3,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def make_tree(path):
-    tree = dict(name=path, children=[])
-    try: lst = os.listdir(path)
-    except OSError:
-        pass #ignore errors
-    else:
-        for name in lst:
-            fn = os.path.join(path, name)
-            if os.path.isdir(fn):
-                tree['children'].append(make_tree(fn))
-            else:
-                tree['children'].append(dict(name=fn))
-    return tree
+# def make_tree(path):
+#     tree = dict(name=path, children=[])
+#     try: lst = os.listdir(path)
+#     except OSError:
+#         pass #ignore errors
+#     else:
+#         for name in lst:
+#             fn = os.path.join(path, name)
+#             if os.path.isdir(fn):
+#                 tree['children'].append(make_tree(fn))
+#             else:
+#                 tree['children'].append(dict(name=fn))
+#     return tree
 
 @app.route('/')
 def index():
-    dir_list = make_tree("/data")
+    # dir_list = make_tree("/data")
+    dir_list = sorted(os.listdir("/data"))
     return render_template('index.html', text=dir_list)
 
+@app.route('/hello')
+def hello():
+    return 'Hello from AFG LAB'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
